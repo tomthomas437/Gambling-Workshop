367d62c03ba14816c9d085427b2adaccfef060b9
tomthomas437@gmail.com <tomthomas437@gmail.com>
2021-11-21 04:41:00 +0530

    Stake of 00 every day and bet  per game

---
+++ Gamble.java
@@ -0,0 +1,11 @@
+package com.gamble;
+
+public class Gamble {
+	public static void main(String[] args) {
+		int StakePerDay = 100;
+		int BetPrice = 1;
+	
+	   System.out.println("The total stake per day is $" + StakePerDay);
+	   System.out.println("Bet price per game is $" + BetPrice);
+	}
+}
